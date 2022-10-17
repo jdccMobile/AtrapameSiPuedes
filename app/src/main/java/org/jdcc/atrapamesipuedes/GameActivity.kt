6987c7b326b16e0f9ff307b0113d70790f60e22c
Jose <josedcastro97@gmail.com>
2022-10-17 22:31:42 +0200

    Generacion aleatoria de piezas y celdas invisibles v2 sin fallos

--- app/src/main/java/org/jdcc/atrapamesipuedes/GameActivity.kt
+++ app/src/main/java/org/jdcc/atrapamesipuedes/GameActivity.kt
@@ -9,6 +9,7 @@ import android.util.TypedValue
 import android.view.View
 import android.widget.ImageView
 import android.widget.TableRow
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_game.*
 
 class GameActivity : AppCompatActivity(), View.OnClickListener {
@@ -17,9 +18,10 @@ class GameActivity : AppCompatActivity(), View.OnClickListener {
     private var msInFuture : Long = 10000           // 10 seconds
     private var countDownInterval : Long = 1000     // 1 second
     private var extraMs : Long = 0
-    private var kingCatched = false
+    private var kingCaught = false
     private lateinit var board : Array<IntArray>
     private var level = 1
+    private var numberCells = 16
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -81,6 +83,7 @@ class GameActivity : AppCompatActivity(), View.OnClickListener {
         var iv : ImageView
         var x = 0
         var y = 0
+
         if(level == 1){ //Initial cells
             for(x in 0..7){
                 for(y in 0..7){
@@ -94,31 +97,22 @@ class GameActivity : AppCompatActivity(), View.OnClickListener {
                 x = (0..7).random()
                 y = (0..7).random()
                 iv = findViewById(resources.getIdentifier("iv_c$x$y","id",packageName))
+                // TODO COMPROBAR QUE ESTE AL LADO DE UNA CELDA VISIBLE
             } while(iv.visibility == View.VISIBLE)
             iv.visibility = View.VISIBLE
+            numberCells++
         }
     }
 
     private fun randomPosition() {
-        var numberOfPieces = 0
         var typePiece = 0
-        var x = 0
-        var y = 0
-        var iv : ImageView
-        numberOfPieces = (5..9).random()    //TODO EL 5 ES LA MITAD DE LAS VISIBLES + 2, EL NUEVE ES EL NUMERO DE CASILLAS VISIBLES
+        var iv: ImageView
+        var i = 0
+        var j = 0
 
-        for(i in 0..numberOfPieces){
-            do {
-                x = (0..7).random()
-                y = (0..7).random()
-            } while(board[x][y] != 0)           //TODO Y QUE LA CASILLA SEA VISIBLE
-
-            iv = findViewById(resources.getIdentifier("iv_c$x$y","id",packageName))
-            if(i == 0){
-                iv.setImageResource(R.drawable.ic_king_game)
-                board[x][y] = 6
-            }
-            else {
+        for (x in 0..7) {                                                                     // Randomize the pieces in the whole board
+            for (y in 0..7) {
+                iv = findViewById(resources.getIdentifier("iv_c$x$y", "id", packageName))
                 typePiece = (0..5).random()
                 when (typePiece) {
                     1 -> {
@@ -143,10 +137,15 @@ class GameActivity : AppCompatActivity(), View.OnClickListener {
                     }
                     else -> board[x][y] = 0
                 }
-
             }
         }
-
+        do{                                                                                         // Set the king's position
+            i = (0..7).random()
+            j = (0..7).random()
+            iv = findViewById(resources.getIdentifier("iv_c$i$j", "id", packageName))
+        } while(iv.visibility != View.VISIBLE)
+        iv.setImageResource(R.drawable.ic_king_game)
+        board[i][j] = 6
     }
 
     private fun startTimer(){
@@ -154,7 +153,7 @@ class GameActivity : AppCompatActivity(), View.OnClickListener {
         countDown = object : CountDownTimer(msInFuture, countDownInterval){
             override fun onTick(millisUntilFinished: Long) {
                 tv_time_bottom.text = (millisUntilFinished / 1000).toString() + " ''"
-                if(kingCatched){
+                if(kingCaught){
                     extraMs = millisUntilFinished
                     countDown.cancel()
                 }
@@ -173,10 +172,90 @@ class GameActivity : AppCompatActivity(), View.OnClickListener {
 
     private fun buttons(){
         iv_c00.setOnClickListener(this)
+        iv_c01.setOnClickListener(this)
+        iv_c02.setOnClickListener(this)
+        iv_c03.setOnClickListener(this)
+        iv_c04.setOnClickListener(this)
+        iv_c05.setOnClickListener(this)
+        iv_c06.setOnClickListener(this)
+        iv_c07.setOnClickListener(this)
+
+        iv_c10.setOnClickListener(this)
+        iv_c11.setOnClickListener(this)
+        iv_c12.setOnClickListener(this)
+        iv_c13.setOnClickListener(this)
+        iv_c14.setOnClickListener(this)
+        iv_c15.setOnClickListener(this)
+        iv_c16.setOnClickListener(this)
+        iv_c17.setOnClickListener(this)
+
+        iv_c20.setOnClickListener(this)
+        iv_c21.setOnClickListener(this)
+        iv_c22.setOnClickListener(this)
+        iv_c23.setOnClickListener(this)
+        iv_c24.setOnClickListener(this)
+        iv_c25.setOnClickListener(this)
+        iv_c26.setOnClickListener(this)
+        iv_c27.setOnClickListener(this)
+
+        iv_c30.setOnClickListener(this)
+        iv_c31.setOnClickListener(this)
+        iv_c32.setOnClickListener(this)
+        iv_c33.setOnClickListener(this)
+        iv_c34.setOnClickListener(this)
+        iv_c35.setOnClickListener(this)
+        iv_c36.setOnClickListener(this)
+        iv_c37.setOnClickListener(this)
+
+        iv_c40.setOnClickListener(this)
+        iv_c41.setOnClickListener(this)
+        iv_c42.setOnClickListener(this)
+        iv_c43.setOnClickListener(this)
+        iv_c44.setOnClickListener(this)
+        iv_c45.setOnClickListener(this)
+        iv_c46.setOnClickListener(this)
+        iv_c47.setOnClickListener(this)
+
+        iv_c50.setOnClickListener(this)
+        iv_c51.setOnClickListener(this)
+        iv_c52.setOnClickListener(this)
+        iv_c53.setOnClickListener(this)
+        iv_c54.setOnClickListener(this)
+        iv_c55.setOnClickListener(this)
+        iv_c56.setOnClickListener(this)
+        iv_c57.setOnClickListener(this)
+
+        iv_c60.setOnClickListener(this)
+        iv_c61.setOnClickListener(this)
+        iv_c62.setOnClickListener(this)
+        iv_c63.setOnClickListener(this)
+        iv_c64.setOnClickListener(this)
+        iv_c65.setOnClickListener(this)
+        iv_c66.setOnClickListener(this)
+        iv_c67.setOnClickListener(this)
+
+        iv_c70.setOnClickListener(this)
+        iv_c71.setOnClickListener(this)
+        iv_c72.setOnClickListener(this)
+        iv_c73.setOnClickListener(this)
+        iv_c74.setOnClickListener(this)
+        iv_c75.setOnClickListener(this)
+        iv_c76.setOnClickListener(this)
+        iv_c77.setOnClickListener(this)
+
+
     }
 
-    override fun onClick(v: View?) {
-        TODO("Not yet implemented")
+    override fun onClick(v: View?) {        // Just clicks on cells
+        var id = v?.tag.toString()
+        var x = id.subSequence(1,2).toString().toInt()
+        var y = id.subSequence(2,3).toString().toInt()
+
+        if(board[x][y] == 6){
+            //TODO AÑADIR PANTALLA PARA EL NUEVO NIVEL
+            val toast = Toast.makeText(this,"OK", Toast.LENGTH_SHORT).show()
+        }
+
     }
 
 
